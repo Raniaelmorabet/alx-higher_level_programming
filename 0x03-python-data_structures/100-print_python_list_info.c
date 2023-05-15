58f10cae09010cf9eba9d6fc24e605489810220a
“Raniaelmorabet” <raniaelmorabet9@g>
2023-05-14 18:11:36 -0700

    task 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,46 +1,33 @@
-#include <Python.h>
+i#include <Python.h>
 #include <stdio.h>
 
-/**
- * print_item_info - Display the information of items of a python list
- *
- * @prmItem: item of a python object
- * @prmItemIndex: item index
- */
 void print_item_info(PyObject *prmItem, int prmItemIndex)
 {
-	char *itemName;
+    char *itemName;
 
-	itemName = (char *)Py_TYPE(prmItem)->tp_name;
+    itemName = (char *)Py_TYPE(prmItem)->tp_name;
 
-	printf("Element %d: %s\n", prmItemIndex, itemName);
-}	
+    printf("Item %d: %s\n", prmItemIndex, itemName);
+}
 
-/**
- * print_python_list_info - display all item informations from one python
- *                          object
- *
- * @p: Python object
- */
-void print_python_list_info(PyObject *p)
+void print_python_tuple_info(PyObject *p)
 {
-	int itemIndex, objAllocatedNb = 0;
-	PyObject *item;
-	Py_ssize_t objListSize = 0;
+    int itemIndex, objAllocatedNb = 0;
+    PyObject *item;
+    Py_ssize_t objTupleSize = 0;
 
-	/* Check if item list is not empty */
-	if (PyList_Check(p))
-	{
-		objListSize = PyList_Size(p);
-		objAllocatedNb = ((PyListObject *)p)->allocated;
+    if (PyTuple_Check(p))
+    {
+        objTupleSize = PyTuple_Size(p);
+        objAllocatedNb = ((PyTupleObject *)p)->ob_base.ob_refcnt - 1;
 
-		printf("[*] Size of the Python List = %d\n", (int) objListSize);
-		printf("[*] Allocated = %d\n", objAllocatedNb);
+        printf("[*] Size of the Python Tuple = %d\n", (int)objTupleSize);
+        printf("[*] Allocated = %d\n", objAllocatedNb);
 
-		for (itemIndex = 0; itemIndex < objListSize; itemIndex++)
-		{
-			item = PyList_GetItem(p, itemIndex);
-			print_item_info(item, itemIndex);
-		}
-	}
+        for (itemIndex = 0; itemIndex < objTupleSize; itemIndex++)
+        {
+            item = PyTuple_GetItem(p, itemIndex);
+            print_item_info(item, itemIndex);
+        }
+    }
 }
