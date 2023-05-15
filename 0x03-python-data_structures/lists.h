5bf91e792d64f5ff709f9973b06d7c776fa63aa7
“Raniaelmorabet” <raniaelmorabet9@g>
2023-05-14 18:13:10 -0700

    header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,7 +18,8 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
 int is_palindrome(listint_t **head);
+void print_item_info(PyObject *prmItem, int prmItemIndex)
+void print_python_tuple_info(PyObject *p)
 
 #endif /* LISTS_H */
