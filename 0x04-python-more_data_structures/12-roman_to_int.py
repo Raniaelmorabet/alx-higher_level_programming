c5c1d8498afcc427be1c4fd4629d6ab5cf61bac0
“Raniaelmorabet” <raniaelmorabet9@g>
2023-05-17 14:28:33 -0700

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_dict[roman_string[i]] > \
+                roman_dict[roman_string[i - 1]]:
+            num += roman_dict[roman_string[i]] - \
+                   2 * roman_dict[roman_string[i - 1]]
+        else:
+            num += roman_dict[roman_string[i]]
+    return num
+
+
+if __name__ == '__main__':
+    roman_string = "I"
+    print(roman_to_int(roman_string))
+
+    roman_string = "X"
+    print(roman_to_int(roman_string))
+
+    roman_string = "VII"
+    print(roman_to_int(roman_string))
+
+    roman_string = "IX"
+    print(roman_to_int(roman_string))
+
+    roman_string = "LXXXVII"
+    print(roman_to_int(roman_string))
