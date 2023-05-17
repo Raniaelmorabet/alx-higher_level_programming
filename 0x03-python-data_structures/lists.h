f3519961ec826795175000931454cefe80357c63
“Raniaelmorabet” <raniaelmorabet9@g>
2023-05-16 19:19:06 -0700

    header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,15 +14,14 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
-void print_item_info(PyObject *prmItem, int prmItemIndex)
-void print_python_tuple_info(PyObject *p)
 
 #endif /* LISTS_H */
