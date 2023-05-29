4d6d32276592a642cac4acd194f7d38d463fe614
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-05-30 00:00:02 +0200

    Update 102-magic_calculation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -14,4 +9,4 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-    return (result)
+    return result
