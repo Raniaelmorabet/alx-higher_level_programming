6eeb142460e4eb6f7668d182d22897387daba11e
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-05-31 00:04:45 +0200

    Delete 1-square.py

--- 0x06-python-classes/1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""This module defines a Square class"""
-
-class square:
-    """Square class with a private instance attribute"""
-    def __init__(self, size):
-        """Initializes the data"""
-        self.__size = size
