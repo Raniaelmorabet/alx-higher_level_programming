3ec314a6df736e81ea06e719aa06145cfb92d106
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-07 19:19:56 +0200

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -92,7 +92,3 @@ def lazy_matrix_mul(m_a, m_b):
             res[i].append(c)
     '''
     return numpy.matrix(m_a) * numpy.matrix(m_b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/101-lazy_matrix_mul.txt")
