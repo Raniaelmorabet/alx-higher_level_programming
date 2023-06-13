13470bfcb392b4fe6ae7bf1d8c8fdf02fd0eecc8
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-14 01:34:17 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
 """
-A script that reads stdin line by line and computes metrics
+A script that reads stdin line by line and computes metrics.
 """
 
 import sys
@@ -10,11 +9,11 @@ import sys
 
 def print_stats(status_codes, file_size):
     """
-    prints stats
+    Prints stats.
 
     Arguments:
-        status_codes {dict} -- [dictionary of status codes]
-        file_size {int} -- [file size]
+        status_codes {dict} -- dictionary of status codes.
+        file_size {int} -- file size.
     """
     print("File size: {}".format(file_size))
     for key, value in sorted(status_codes.items()):
@@ -34,11 +33,11 @@ if __name__ == "__main__":
             split_line = line.split()
             try:
                 file_size += int(split_line[-1])
-            except [ValueError, IndexError]:
+            except (ValueError, IndexError):
                 pass
             try:
                 status_codes[split_line[-2]] += 1
-            except [KeyError, IndexError]:
+            except (KeyError, IndexError):
                 pass
             if count % 10 == 0:
                 print_stats(status_codes, file_size)
