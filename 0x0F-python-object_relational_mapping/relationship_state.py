c619e81d583969e4505d29feadfd8748ea0cff62
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-08-20 20:58:04 +0200

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-contains the class definition of a State and an instance Base
-"""
+"""Defines a class State"""
+
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Representation of a state"""
+    """Module of a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
